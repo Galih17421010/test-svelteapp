b73cd9194a163c1af4f17a1ffcab4456d7cc4ce1
Galih Agus Saputra <agussaputragalih@gmail.com>
2024-03-31 13:44:57 +0700

    Update file

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,29 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const base = '/test-app';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({ fallback: '404.html' }),
+    alias: {
+			$lib: './src/lib',
+			'@data': './src/lib/data',
+			'@components': './src/lib/components',
+			'@md': './src/lib/md',
+			'@stores': './src/lib/stores',
+			'@utils': './src/lib/utils'
+		},
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? base : ''
+		}
   },
-  preprocess: vitePreprocess()
+  preprocess: vitePreprocess(),
+  vitePlugin: {
+		inspector: {
+			showToggleButton: 'always'
+		}
+	},
 };
 export default config;
